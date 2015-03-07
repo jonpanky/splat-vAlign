9252468bd63a93f5b111098756a92ba15620c3ed
Jon Pankhurst <jon.pankhurst@photo4me.com>
2015-03-07 17:38:41 +0000

    V 1

---
+++ Scripts/Splat.js
@@ -0,0 +1,20 @@
+﻿var Splat = function () {
+
+    return {
+        ///asdm alslasjr
+        vAlign: function vAlign($imgOrDivToReSize, $imgOrDivToFindDimensions, dblScaleOfResize, cb) {
+
+            try {
+                dblScaleOfResize = typeof dblScaleOfResize !== 'undefined' ? dblScaleOfResize : 1;
+                var h = $imgOrDivToFindDimensions.height() * dblScaleOfResize;
+                $imgOrDivToReSize.css('height', h).css('vertical-align', 'middle').css('display', 'table-cell').css('width', '1000');
+
+                cb(true);
+            } catch (err) {
+                cb(false);
+            }
+
+        }
+    };
+
+};
